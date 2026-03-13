761d98ea05ef1ca88ac938cc086bc3071098e9d7
Arthur-dev-br <kaponcio52@gmail.com>
2026-03-13 14:10:24 -0300

    Add files via upload

---
+++ eventos.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+<!-- Descrição do site - SEO e compartilhamento -->
+
+<meta name="description" content="Mais do que servir café, é um ambiente que celebra o ofício artesanal, a cultura do café brasileiro e as histórias por trás de cada grão.
+Cada xícara é uma narrativa do produtor ao barista, do barista ao cliente. Aqui, o tempo desacelera, os sentidos despertam e o café volta a ser o que sempre foi: um ritual de cuidado e partilha.">
+ <!-- Palavra Chave -->
+
+  <meta name="keywords" content=" café, barista, café brasileiro, artesanal, grão">
+
+  <!-- Autor do site -->
+
+   <meta name="autor"  content="Arthur Costa">
+   
+   <!-- Open Graph (Compartilhar no Whats, Face, X, LinkedIn)-->
+
+    <meta property="og:title" content="CASA DO BARISTA">
+    <meta property="og:description"
+    content="Mais do que servir café, é um ambiente que celebra o ofício artesanal, a cultura do café brasileiro e as histórias por trás de cada grão.
+    Cada xícara é uma narrativa do produtor ao barista, do barista ao cliente. Aqui, o tempo desacelera, os sentidos despertam e o café volta a ser o que sempre foi: um ritual de cuidado e partilha.">
+    <meta property="og:image" content="https://">
+    <meta property="og:type" content="website">
+
+    <!-- Links dos ICONS para FAVICOM -->
+
+    <link rel="apple-touch-icon" sizes="57x57" href="assets/icon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="assets/icon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="assets/icon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/icon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="assets/icon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="assets/icon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="assets/icon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="assets/icon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/icon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="assets/icon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/icon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="assets/icon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/icon/favicon-16x16.png">
+    <link rel="manifest" href="assets/icon/manifest.json">
+    <meta name="msapplication-TileColor" content="#8b5e3c">
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <meta name="theme-color" content="#8b5e3c">
+    
+<title>CASA DO BARISTA</title>
+<!-- Reset limpa os estilos -->
+<link rel="stylesheet" href="css/reset.css">
+
+<!-- Slick -->
+
+<link rel="stylesheet" href="css/slick.css">
+
+<link rel="stylesheet" href="css/slick-theme.css">
+<!-- Animação - Animate CSS -->
+<link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+  />
+</head>
+
+<!-- estilo adiciona -->
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/responsive.css">
+
+
+
+<body>
+  <!-- inicio Cabeçalho -->
+
+   <?php require_once ('conteudo/topo.php'); ?>
+
+ 
+  <!-- Fim cabeçalho -->
+
+  <!-- Corpo -->
+   <main>
+  
+     <!-- Eventos inicio -->
+
+      <?php require_once ('conteudo/eventos.php'); ?>
+
+     <!-- eventos fim -->
+
+
+     <!-- Depoimentos inicio -->
+
+     <?php require_once ('conteudo/depoimentos.php'); ?>
+
+     <!-- Depoimentos Fim -->
+
+
+
+
+     
+
+     
+
+
+
+
+
+   </main>
+
+
+     <!-- Inicio-Rodapé -->
+
+     <?php require_once ('conteudo/rodape.php'); ?>
+
+
+
+     
+     <!-- Fim-Rodapé -->
+
+
+
+
+
+
+      <!-- JQUERY -->
+   <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+
+   <!-- SLICK -->
+   <script src="js/slick.min.js"></script>
+
+   <!-- ANIMAÇÃO WOW -->
+
+   <script src="js/wow.min.js"></script>
+
+   <!-- Meu script animação -->
+
+
+   <script src="js/script.js"></script>
+
+
+
+
+
+</body>
+
+
+
+
+
+
+</html>
