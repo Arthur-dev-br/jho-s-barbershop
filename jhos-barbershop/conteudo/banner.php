6b55a50fc4b2ef688af62619d2867906cd090c98
Arthur-dev-br <kaponcio52@gmail.com>
2026-03-17 14:31:24 -0300

    Add files via upload

--- jhos-barbershop/conteudo/banner.php
+++ jhos-barbershop/conteudo/banner.php
@@ -1,5 +1,4 @@
  <section class="banner">
-            <img src="assets/Banner_teste.jpg" alt="Banner do site - Jho's Barbershop">
-          
-
+            <img src="assets/banner_definitivo_jho.png" alt="Banner do site - Jho's Barbershop">
+            <img src="assets/banner_definitivo_jho2.png" alt="Banner do site - Jho's Barbershop">
         </section>
--- jhos-barbershop/conteudo/galeria.php
+++ jhos-barbershop/conteudo/galeria.php
@@ -1,50 +1,79 @@
  <section class="galeria">
-            <header class="parallax-padrao">
-                <h2>
-                    Galeria
-                </h2>
-                <h3>
-                    Veja os resultados de nossos clientes!
-                </h3>
-            </header>
-            <div class="cards-galeria wow animate__animated animate__fadeInUp">
+     <header class="parallax-padrao">
+         <h2>
+             Galeria
+         </h2>
+         <h3>
+             Veja os resultados de nossos clientes!
+         </h3>
+     </header>
+     <div class="site-card cardGaleria slideGaleria wow animate__animated animate__fadeInUp">
 
-                <article>
-                    <img src="assets/galeria_foto1.png" alt="Imagem de um cliente após cortar o cabelo">
-                    
+         <article>
+             <img src="assets/foto_galeria1.png" alt="Imagem de um cliente após cortar o cabelo">
 
-                </article>
 
-                <article>
-                    <img src="assets/galeria_foto2.png" alt="Imagem de um cliente após cortar o cabelo">
-                   
+         </article>
 
-                </article>
+         <article>
+             <img src="assets/foto_galeria2.png" alt="Imagem de um cliente após cortar o cabelo">
 
-                <article>
-                    <img src="assets/galeria_foto3.png" alt="Imagem de um cliente após cortar o cabelo">
-                    
 
-                </article>
+         </article>
 
-                <article>
-                    <img src="assets/galeria_foto1.png" alt="Imagem de um cliente após cortar o cabelo">
-                   
+         <article>
+             <img src="assets/foto_galeria3.png" alt="Imagem de um cliente após cortar o cabelo">
 
-                </article>
 
-                <article>
-                    <img src="assets/galeria_foto2.png" alt="Imagem de um cliente após cortar o cabelo">
-                    
+         </article>
 
-                </article>
+         <article>
+             <img src="assets/foto_galeria4.png" alt="Imagem de um cliente após cortar o cabelo">
 
-                <article>
-                    <img src="assets/galeria_foto3.png" alt="Imagem de um cliente após cortar o cabelo">
-                    
 
-                </article>
+         </article>
 
-            </div>
-            
-        </section>
+         <article>
+             <img src="assets/foto_galeria5.png" alt="Imagem de um cliente após cortar o cabelo">
+
+
+         </article>
+
+         <article>
+             <img src="assets/foto_galeria6.png" alt="Imagem de um cliente após cortar o cabelo">
+
+         </article>
+
+         <article>
+          <img src="assets/foto_galeria7.png" alt="Imagem de um cliente após cortar o cabelo">
+
+
+        </article>
+
+        <article>
+          <img src="assets/foto_galeria8.png" alt="Imagem de um cliente após cortar o cabelo">
+
+
+        </article>
+
+        <article>
+          <img src="assets/foto_galeria9.png" alt="Imagem de um cliente após cortar o cabelo">
+
+
+        </article>
+
+        <article>
+          <img src="assets/foto_galeria10.png" alt="Imagem de um cliente após cortar o cabelo">
+
+
+        </article>
+
+        <article>
+          <img src="assets/foto_galeria11.png" alt="Imagem de um cliente após cortar o cabelo">
+
+
+        </article>
+
+     </div>
+
+ </section>
