bf7ec5d1bc687483f5aa57715785c2ecd00e52e4
Arthur-dev-br <kaponcio52@gmail.com>
2026-03-17 10:44:07 -0300

    Delete conteudo directory

--- conteudo/banner.php
+++
@@ -1,4 +0,0 @@
- <section class="banner">
-            <img src="assets/banner_definitivo_jho.png" alt="Banner do site - Jho's Barbershop">
-            <img src="assets/banner_definitivo_jho2.png" alt="Banner do site - Jho's Barbershop">
-        </section>
