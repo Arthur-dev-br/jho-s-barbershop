65470004eeef0d426344cfaf8bb247c23169c350
Arthur-dev-br <kaponcio52@gmail.com>
2026-03-19 17:13:15 -0300

    Add files via upload

--- jhos-barbershop/conteudo/produtos.php
+++ jhos-barbershop/conteudo/produtos.php
@@ -1,45 +1,106 @@
- <section class="produtos">
-            <header class="parallax-padrao">
-                <h2>
-                    Produtos
-                </h2>
-                <h3>
-                    kits personalizados para seu cuidado!
-                </h3>
+<section class="produtos">
+    <header class="parallax-padrao">
+        <h2>
+            Produtos
+        </h2>
+        <h3>
+            kits personalizados para seu cuidado!
+        </h3>
 
-            </header>
+    </header>
 
-            <div class="site card-produtos">
+    <div class="site card-produtos">
 
-                <div class="card-flip wow animate__animated animate__fadeInUp animate__delay-1s">
-                    <article class="card-flip-miolo">
-                        
-                        <div class="flip1">
-                            <img src="assets/produtos_1.png" alt="imagem de um kit de produtos para cabelo">
-                            <h4>
-                                pomadas “fox for men”
-                            </h4>
-                        </div>
-    
-                        <div class="flip2">
-                            <h5>
-                                R$ 35,00
-                            </h5>
-                            <p>
-                                Pomadas para cabelo
-                            </p>
-                            
-                        </div>
-                    </article>
+        <div class="card-flip wow animate__animated animate__fadeInUp animate__delay-1s">
+            <article class="card-flip-miolo">
 
+                <div class="flip1">
+                    <img src="assets/pomada_premium.png" alt="imagem de um kit de produtos para cabelo">
+                    <h4>
+                        pomada extra forte “fox for men”
+                    </h4>
                 </div>
 
-                
-                
+                <div class="flip2">
 
-            </div>
+                    <h4>
+                        pomada extra forte “fox for men”
+                    </h4>
+                    <h4>
+                        R$ 35,00
+                    </h4>
+                    <p>
+                        Pomadas para cabelo
+                    </p>
 
-             <div class="mais wow animate__animated animate__fadeInUp">
-               <a href="servicos.php"> Veja Mais</a>
-             </div>
-        </section>
+                </div>
+            </article>
+
+
+        </div>
+
+        <div class="card-flip wow animate__animated animate__fadeInUp animate__delay-1s">
+            <article class="card-flip-miolo">
+
+                <div class="flip1">
+                    <img src="assets/pomada_matte.png" alt="imagem de um kit de produtos para cabelo">
+                    <h4>
+                        pomada matte “fox for men”
+                    </h4>
+                </div>
+
+                <div class="flip2">
+                    <h4>
+                        pomada matte “fox for men”
+                    </h4>
+                    <h4>
+                        R$ 35,00
+                    </h4>
+                    <p>
+                        cera modeladora efeito matte
+                    </p>
+
+                </div>
+            </article>
+
+
+        </div>
+
+
+        <div class="card-flip wow animate__animated animate__fadeInUp animate__delay-1s">
+            <article class="card-flip-miolo">
+
+                <div class="flip1">
+                    <img src="assets/pomada_teia.png" alt="imagem de um kit de produtos para cabelo">
+                    <h4>
+                        pomada teia “fox for men”
+                    </h4>
+                </div>
+
+                <div class="flip2">
+                    <h4>
+                        pomada teia “fox for men”
+                    </h4>
+                    <h4>
+                        R$ 35,00
+                    </h4>
+                    <p>
+                        creme modelador efeito teia
+                    </p>
+
+                </div>
+            </article>
+
+
+        </div>
+
+
+
+
+
+    </div>
+
+    <div class="mais wow animate__animated animate__fadeInUp">
+        <a href="servicos.php"> Veja Mais</a>
+    </div>
+</section>
