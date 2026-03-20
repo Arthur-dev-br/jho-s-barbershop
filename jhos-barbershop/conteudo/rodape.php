ad465bda599b01e0d3cbff87f95034d03a6b83cd
Arthur-dev-br <kaponcio52@gmail.com>
2026-03-20 17:13:57 -0300

    Add files via upload

--- jhos-barbershop/conteudo/rodape.php
+++ jhos-barbershop/conteudo/rodape.php
@@ -1,31 +1,31 @@
 <footer class="rodape">
 
-        <section class="site rodape-grid">
+  <section class="site rodape-grid">
 
-            <div class="coluna-logo">
+    <div class="coluna-logo">
 
-        <h1>Jho's Barbershop</h1>
+      <h1>Jho's Barbershop</h1>
 
-        </div>
- 
-        <div class="box-rede">
-             <ul class="rede-social">
+    </div>
+
+    <div class="box-rede">
+      <ul class="rede-social">
         <li><a href="#" target="_blank"><img src="assets/instagram-24.png" alt="Logo do instagram 60x60"></a></li>
-     </ul>
+      </ul>
 
-        </div>
-      </section>
+    </div>
+  </section>
 
 
-      
-      <!-- <div class="creditos">
+
+  <!-- <div class="creditos">
           <h2>
-              <?php  $data = date('Y');
-          echo $data ?>
+              <?php $data = date('Y');
+              echo $data ?>
           -Criado e desenvolvido por Arthur Costa - Senac Smp
         </h2>
         
       </div> -->
-    
-    
+
+
 </footer>
